e971215ec6b22eba9ff85e89d64933383e32ac2f
Chase Garner <ng0089@uah.edu>
2026-07-31 13:27:46 -0500

    Throw error when JWT_SECRET is not configured

--- apps/api/src/config/env.ts
+++ apps/api/src/config/env.ts
@@ -3,10 +3,14 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: path.resolve(__dirname, "../../../../.env") });
 
+if (!process.env.JWT_SECRET) {
+	console.log("Change the JWT_SECRET environment variable!");
+}
+
 export const env = {
 	port: Number(process.env.PORT || 3000),
 	databaseUrl:
 		process.env.DATABASE_URL ||
 		"postgresql://postgres:postgres@localhost:5432/cs453",
-	jwtSecret: process.env.JWT_SECRET || "development"
+	jwtSecret: process.env.JWT_SECRET || "development_change_me"
 };
